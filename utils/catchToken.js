204a321093df844fc9fb638d1cd7ca722fdb3bbe
meAyushSharma <iamayushsharma60@gmail.com>
2024-08-31 14:19:59 +0530

    improved ui

--- utils/catchToken.js
+++ utils/catchToken.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 // const cookieParser = require('cookie-parser');
 
 module.exports.isTokenAndValid = async function (req, res) {
-  // console.log("welcome to isTokenValid")
   const token = req.cookies.token;
   const googleToken = req.cookies.googleToken;
   const passkeyToken = req.cookies.passkeyToken;
@@ -25,11 +24,8 @@ module.exports.isTokenAndValid = async function (req, res) {
 if(googleToken){
   const decoded = jwt.verify(googleToken, process.env.JWT_SECRET_KEY);
   const {username, name} = decoded;
-  // console.log("decoded google token is ----------------------------  ", decoded);
   const userExist = await User.findOne({username: username});
-  // console.log("userExist is:  ", userExist);
   if(!userExist){
-    // console.log("returning null....")
     return null;
   }
   return userExist;
